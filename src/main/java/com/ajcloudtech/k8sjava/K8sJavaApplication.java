0a9da948c4deb1c48a17bd18922a0b16bc77e7e5
akshayjavvaji14 <akshayjavvaji14@gmail.com>
2020-07-11 23:26:25 -0700

    Added additional request mapings

--- src/main/java/com/ajcloudtech/k8sjava/K8sJavaApplication.java
+++ src/main/java/com/ajcloudtech/k8sjava/K8sJavaApplication.java
@@ -2,6 +2,7 @@ package com.ajcloudtech.k8sjava;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,7 +18,10 @@ public class K8sJavaApplication {
     public String index() {
         return "Hello to you from springBootExample";
     }
-
+	@RequestMapping("/hello/{name}")
+  	String hello(@PathVariable String name) {
+    return "Hello, " + name + "!";
+  }
     
 
 }
